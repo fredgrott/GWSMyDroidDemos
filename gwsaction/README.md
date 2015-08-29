fda9c75830a81a52519a386d3706316e718e99ce
Fred Grott <fred.grott@gmail.com>
2015-08-29 05:37:03 -0500

    added action

---
+++ gwsaction/README.md
@@ -0,0 +1,8 @@
+
+
+
+Credits
+=======
+
+
+[markushi android-ui]()
---
+++ gwsaction/src/main/java/com/grottworkshop/gwsaction/action/CloseAction.java
@@ -0,0 +1,20 @@
+package com.grottworkshop.gwsaction.action;
+
+/**
+ * Created by fgrott on 8/29/2015.
+ */
+public class CloseAction extends Action {
+
+    public CloseAction() {
+        final float start = 0.239375f;
+        final float end = 1f - start;
+
+        lineData = new float[]{
+                // line 1
+                start, start, end, end,
+                // line 2
+                start, end, end, start,
+                // line 3
+                start, start, end, end,};
+    }
+}
