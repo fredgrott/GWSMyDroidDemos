14afdb9ee67dbc73b38b28dd8b6c9aadae4a2a5f
Fred Grott <fred.grott@gmail.com>
2015-09-01 07:18:26 -0500

    added dsicrollview demo

---
+++ gwscirclerevealdemo/src/main/java/com/grottworkshop/gwscirclerevealdemo/MainActivity.java
@@ -0,0 +1,9 @@
+package com.grottworkshop.gwscirclerevealdemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fgrott on 9/1/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+}
---
+++ gwsdiscrollviewdemo/src/main/java/com/grottworkshop/gwsdiscrollviewdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.grottworkshop.gwsdiscrollviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fgrott on 9/1/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
