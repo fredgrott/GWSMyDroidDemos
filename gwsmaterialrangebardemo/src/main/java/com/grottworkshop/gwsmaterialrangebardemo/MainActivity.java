250693f79955d26f45b5fe916f228b25851422d4
Fred Grott <fred.grott@gmail.com>
2015-09-07 08:52:27 -0500

    first commit of materialrangebardemo

---
+++ gwsmaterialrangebardemo/src/main/java/com/grottworkshop/gwsmaterialrangebardemo/MainActivity.java
@@ -0,0 +1,9 @@
+package com.grottworkshop.gwsmaterialrangebardemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fgrott on 9/7/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+}
