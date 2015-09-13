010d496facea2aba2c4708d1368cd39429d1202b
Fred Grott <fred.grott@gmail.com>
2015-09-13 07:43:30 -0500

    added multistateviewdemo

---
+++ gwsmultistateviewdemo/src/main/java/com/grottworkshop/gwsmultistateviewdemo/MainActivity.java
@@ -0,0 +1,82 @@
+package com.grottworkshop.gwsmultistateviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.grottworkshop.gwsmultistateview.MultiStateView;
+
+/**
+ * Created by fgrott on 9/13/2015.
+ */
+public class MainActivity extends AppCompatActivity {
+    MultiStateView.ContentState mState;
+
+    private MultiStateView mMultiStateView;
+
+    private TextView mExampleOfHowToGetContentView;
+
+    private TextView mStateView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStateView = (TextView) findViewById(R.id.state);
+        mMultiStateView = (MultiStateView) findViewById(R.id.content);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        mState = mMultiStateView.getState();
+        mExampleOfHowToGetContentView = (TextView) mMultiStateView.getContentView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setStateText(mState);
+    }
+
+    private void setStateText(MultiStateView.ContentState state) {
+        mStateView.setText(String.format("State: %s", state));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_rotate_state) {
+            // This is only done because we're rotating state; you'd typically just call direct to mMultiStateView#setState(ContentState)
+            MultiStateView.ContentState newState = MultiStateView.ContentState.values()[(mState.ordinal() + 1) % MultiStateView.ContentState.values().length];
+
+            setState(newState);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setState(MultiStateView.ContentState state) {
+        setStateText(state);
+        mMultiStateView.setState(state);
+        mState = state;
+    }
+
+}
