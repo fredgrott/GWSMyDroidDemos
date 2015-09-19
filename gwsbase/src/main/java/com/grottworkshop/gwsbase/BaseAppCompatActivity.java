e66f04c70aab528c262181296f51ab05d72984c2
Fred Grott <fred.grott@gmail.com>
2015-09-19 15:32:43 -0500

    added more to gwsbase...BaseCompatActivity and re-orgnaized methods to they are in lifecycle execution order

--- gwsbase/src/main/java/com/grottworkshop/gwsbase/BaseAppCompatActivity.java
+++ gwsbase/src/main/java/com/grottworkshop/gwsbase/BaseAppCompatActivity.java
@@ -1,21 +1,41 @@
 package com.grottworkshop.gwsbase;
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnActivityResultEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnAttachFragmentEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnAttachToWindowEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnConfigurationChangedEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnContentChangedEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnCreateEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnCreateOptionsMenuEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnDestroyEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnLowMemoryEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnPauseEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnPostCreateEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnPostResumeEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnPrepareOptionsMenuEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnRestartEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnRestoreInstanceStateEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnResumeEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnSaveInstanceStateEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnStartEvent;
 import com.grottworkshop.gwsbase.bus.AppCompatActivityOnStateNotSavedEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnStopEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnUserInteractionEvent;
+import com.grottworkshop.gwsbase.bus.AppCompatActivityOnUserLeaveHintEvent;
 import com.grottworkshop.gwsviewserver.ViewServer;
 
 import timber.log.Timber;
+
+import static com.grottworkshop.gwsbase.BaseApplication.getBus;
+import static com.grottworkshop.gwsbase.BaseApplication.myDebugMode;
 
 /**
  * BaseAppCompatActivity class
@@ -25,33 +45,82 @@ public class BaseAppCompatActivity extends AppCompatActivity {
 
     public String TAG = "BaseAppCompatActivity";
 
+    //main lifecycle methods in order of lifecycle
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         Timber.tag(TAG);
-        BaseApplication.getBus().post(new AppCompatActivityOnCreateEvent());
+        getBus().post(new AppCompatActivityOnCreateEvent());
         //vewserver call on debug builds
-        if(BaseApplication.myDebugMode){
+        if(myDebugMode){
             ViewServer.get(this).addWindow(this);
             Timber.d("register view hierarchy with view server");
         }
         initOnCreateBody();
     }
 
-
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        getBus().post(new AppCompatActivityOnCreateEvent());
+        Timber.tag(TAG);
+        Timber.d("onCreate");
+        initOnCreateBodyTwo();
+    }
 
     @Override
-    public void onDestroy(){
-        super.onDestroy();
-        //unregister the view hierarchy with the view server
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+        getBus().post(new AppCompatActivityOnAttachFragmentEvent());
         Timber.tag(TAG);
-        if(BaseApplication.myDebugMode){
-            ViewServer.get(this).removeWindow(this);
-            Timber.d("unregister view hierarchy with view server");
-        }
-        initOnDestroyBody();
-        //post out onDestroy event to bus
-        BaseApplication.getBus().post(new AppCompatActivityOnDestroyEvent());
+        Timber.d("onAttachFragment");
+        initOnAttachFragmentBody();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        getBus().post(new AppCompatActivityOnStartEvent());
+        Timber.tag(TAG);
+        Timber.d("onStart");
+        initOnStartBody();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        getBus().post(new AppCompatActivityOnRestartEvent());
+        Timber.tag(TAG);
+        Timber.d("onRestart");
+        initOnRestartBody();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        getBus().post(new AppCompatActivityOnActivityResultEvent());
+        Timber.tag(TAG);
+        Timber.d("onActivityResult");
+        initOnActivityResultBody();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        getBus().post(new AppCompatActivityOnRestoreInstanceStateEvent());
+        Timber.tag(TAG);
+        Timber.d("onRestoreInstanceSte");
+        initOnRestoreInstanceStateBody();
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        getBus().post(new AppCompatActivityOnPostCreateEvent());
+        Timber.tag(TAG);
+        Timber.d("onPostCreate");
+        initOnPostCreateBody();
     }
 
     @Override
@@ -59,56 +128,130 @@ public class BaseAppCompatActivity extends AppCompatActivity {
         super.onResume();
         Timber.tag(TAG);
         //register focus with view server
-        if(BaseApplication.myDebugMode){
+        if(myDebugMode){
             ViewServer.get(this).setFocusedWindow(this);
             Timber.d("register view focus with view server");
         }
         initOnResumeBody();
         //post our onResume event to bus
-        BaseApplication.getBus().post(new AppCompatActivityOnResumeEvent());
+        getBus().post(new AppCompatActivityOnResumeEvent());
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        BaseApplication.getBus().post(new AppCompatActivityOnConfigurationChangedEvent());
+    protected void onPostResume() {
+        super.onPostResume();
+        getBus().post(new AppCompatActivityOnPostResumeEvent());
         Timber.tag(TAG);
-        Timber.d("onConfigurationChanged");
-        initOnConfigurationChangedBody();
+        Timber.d("onPostResume");
+        initOnPostResumeBody();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        getBus().post(new AppCompatActivityOnAttachToWindowEvent());
+        Timber.tag(TAG);
+        Timber.d("onAttachToWindow");
+        initOnAttachToWindowBody();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getBus().post(new AppCompatActivityOnCreateOptionsMenuEvent());
+        Timber.tag(TAG);
+        Timber.d("onCreateOptionsMenu");
+        initOnCreateOptionsMenuBody();
+        return super.onCreateOptionsMenu(menu);
 
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        BaseApplication.getBus().post(new AppCompatActivityOnRestartEvent());
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getBus().post(new AppCompatActivityOnPrepareOptionsMenuEvent());
         Timber.tag(TAG);
-        Timber.d("onRestart");
-        initOnRestartBody();
+        Timber.d("onPrepareOptionsMenu");
+        initOnPrepareOptionsMenuBody();
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        BaseApplication.getBus().post(new AppCompatActivityOnLowMemoryEvent());
+    public void onUserInteraction() {
+        super.onUserInteraction();
+        getBus().post(new AppCompatActivityOnUserInteractionEvent());
         Timber.tag(TAG);
-        Timber.d("onLowMemory");
-        initOnLowMemoryBody();
+        Timber.d("onUserInteraction");
+        initOnUserInteractionBody();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        BaseApplication.getBus().post(new AppCompatActivityOnCreateEvent());
+    protected void onUserLeaveHint() {
+        super.onUserLeaveHint();
+        getBus().post(new AppCompatActivityOnUserLeaveHintEvent());
         Timber.tag(TAG);
-        Timber.d("onCreate");
-        initOnCreateBodyTwo();
+        Timber.d("onUserLeaveHint");
+        initOnUserLeaveHintBody();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getBus().post(new AppCompatActivityOnPauseEvent());
+        Timber.tag(TAG);
+        Timber.d("onPause");
+        initOnPauseBody();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getBus().post(new AppCompatActivityOnSaveInstanceStateEvent());
+        Timber.tag(TAG);
+        Timber.d("onSaveInstanceState");
+        initOnSaveInstanceStateBody();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        getBus().post(new AppCompatActivityOnSaveInstanceStateEvent());
+        Timber.tag(TAG);
+        Timber.d("onSaveInstanceState");
+        initOnSaveInstanceStateBodyTwo();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getBus().post(new AppCompatActivityOnStopEvent());
+        Timber.tag(TAG);
+        Timber.d("onStop");
+        initOnStopBody();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        //unregister the view hierarchy with the view server
+        Timber.tag(TAG);
+        if(myDebugMode){
+            ViewServer.get(this).removeWindow(this);
+            Timber.d("unregister view hierarchy with view server");
+        }
+        initOnDestroyBody();
+        //post out onDestroy event to bus
+        getBus().post(new AppCompatActivityOnDestroyEvent());
+    }
+
+
+    //other mis lifecycle methods
 
     @Override
     public void onContentChanged(){
         super.onContentChanged();
-        BaseApplication.getBus().post(new AppCompatActivityOnContentChangedEvent());
+        getBus().post(new AppCompatActivityOnContentChangedEvent());
         Timber.tag(TAG);
         Timber.d("onContentChanged");
         initOnContentChangedBody();
@@ -116,13 +259,150 @@ public class BaseAppCompatActivity extends AppCompatActivity {
     }
 
 
+
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        getBus().post(new AppCompatActivityOnConfigurationChangedEvent());
+        Timber.tag(TAG);
+        Timber.d("onConfigurationChanged");
+        initOnConfigurationChangedBody();
+
+    }
+
+
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        getBus().post(new AppCompatActivityOnLowMemoryEvent());
+        Timber.tag(TAG);
+        Timber.d("onLowMemory");
+        initOnLowMemoryBody();
+    }
+
+
+
+
+
+
     @Override
     public void onStateNotSaved(){
         super.onStateNotSaved();
-        BaseApplication.getBus().post(new AppCompatActivityOnStateNotSavedEvent());
+        getBus().post(new AppCompatActivityOnStateNotSavedEvent());
         Timber.tag(TAG);
         Timber.d("onStateNotSaved");
         initOnStateNotSavedBody();
+    }
+
+    /**
+     * initOnStopBoyd method container
+     */
+    public void initOnStopBody(){
+
+    }
+
+    /**
+     * initOnSaveInstanceStateBodyTwo method container
+     */
+    public void initOnSaveInstanceStateBodyTwo(){}
+
+
+    /**
+     * initOnSaveInstanceStateBody method container
+     */
+    public void initOnSaveInstanceStateBody(){}
+
+
+
+
+    /**
+     * initOnPauseBody method container
+     */
+    public void initOnPauseBody(){
+
+    }
+
+    /**
+     * initOnUserInteractionBody method container
+     */
+    public void initOnUserInteractionBody(){
+
+    }
+
+
+    /**
+     * initOnUserLeaveHintBody method container
+     */
+    public void initOnUserLeaveHintBody(){
+
+    }
+
+    /**
+     * initOnPrepareOptionsMenuBody method container
+     */
+    public void initOnPrepareOptionsMenuBody(){
+
+    }
+
+
+
+    /**
+     * initOnCreateOptionsMenuBody method container
+     */
+    public void initOnCreateOptionsMenuBody(){
+
+    }
+
+    /**
+     * initOnAttachToWindowBody method container
+     */
+    public void initOnAttachToWindowBody(){
+
+    }
+
+    /**
+     * initOnPostResumeBody method container
+     */
+    public void initOnPostResumeBody(){
+
+    }
+
+
+    /**
+     * initOnPostCreateBody method container
+     */
+    public void initOnPostCreateBody(){
+
+    }
+
+    /**
+     * initOnRestoreInstanceStateBody method container
+     */
+    public void initOnRestoreInstanceStateBody(){
+
+    }
+
+    /**
+     * initOnActivityResultBody method container
+     */
+    public void initOnActivityResultBody(){}
+
+
+    /**
+     * initOnStartBody method container
+     */
+    public void initOnStartBody(){
+
+    }
+
+    /**
+     * initOnAttachFragmentBody method container
+     */
+    public void initOnAttachFragmentBody(){
+
     }
 
     /**
