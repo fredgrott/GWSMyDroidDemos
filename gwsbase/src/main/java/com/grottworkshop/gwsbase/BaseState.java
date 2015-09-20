121d89c42e9cca83f0e943331bb2c6a2ea5bd187
Fred Grott <fred.grott@gmail.com>
2015-09-19 19:30:29 -0500

    added more to gwsbase...added BaseState stub class as we do need a global savedstate singleton

---
+++ gwsbase/src/main/java/com/grottworkshop/gwsbase/BaseState.java
@@ -0,0 +1,10 @@
+package com.grottworkshop.gwsbase;
+
+/**
+ * BaseState class
+ * Created by fgrott on 9/19/2015.
+ */
+@SuppressWarnings("unused")
+public class BaseState {
+    //TODO: need a global state class and have it integrate with BaseApplication usecase is in backgrounded apps GC'd the resulting restarted app will not have updated state via the mainactivity
+}
