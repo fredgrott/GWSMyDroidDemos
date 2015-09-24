d5541d447777f166997305d502ce9d1f01379872
Fred Grott <fred.grott@gmail.com>
2015-09-24 07:07:10 -0500

    added to gwsbase BaseSavedprefs stub

---
+++ gwsbase/src/main/java/com/grottworkshop/gwsbase/BaseSavedPref.java
@@ -0,0 +1,10 @@
+package com.grottworkshop.gwsbase;
+
+/**
+ * BaseeSavedPref, over-riddable class to group classes with prefs fields annotated to
+ * save and restore using PreferencesManager class from gwsutil.
+ * Created by fgrott on 9/24/2015.
+ */
+@SuppressWarnings("unused")
+public class BaseSavedPref {
+}
